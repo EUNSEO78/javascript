dd949f92bdfb820f38f47c9bc1c5a826ce295ca2
EUNSEO78 <yyy3182@naver.com>
2024-07-19 14:55:00 +0900

    Add files via upload

---
+++ javascript/Day01/hello.js
@@ -0,0 +1,12 @@
+// window.alert("오늘 자바스크립트 배우는 날");
+
+//window.alert("오늘 점심은 닭가슴살과 김밥");
+
+//window.console.log("주말에는 친구랑 서울가서 고기 먹을거임");
+
+//window.console.log("주말 계획: 서울감");
+//window.console.log("오늘 저녁 메뉴: 야구장 간식들");
+//window.console.log("자바스크립트 후기: 아직까지는 괜찮음");
+
+
+
---
+++ javascript/Day01/memory/index.js
@@ -0,0 +1,22 @@
+const name = "최은서";
+const coffee = "아메리카노";
+
+// window.console.log(name);
+// window.console.log(coffee);
+// window.console.log("제 이름은 name 좋아하는 커피는 coffee 입니다.");
+// window.console.log(`제 이름은 ${name} 좋아하는 커피는 ${coffee} 입니다.`);
+
+const mbti = "esfp";
+const weekend = "서울";
+const youtube = "먹방";
+
+// window.console.log(
+//   `제 mbti는 ${mbti}이구요, 주말에 ${weekend}가고요, 유튜브는 ${youtube}을 볼거에요!`
+// );
+
+const phone = window.prompt("당신의 핸드폰 기종은?");
+const pc = window.prompt("당신의 PC 기종은?");
+
+window.console.log(
+  `당신이 쓰는 핸드폰은 ${phone}이고, 당신의 PC는 ${pc}이군요!`
+);
