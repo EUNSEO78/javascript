d03dc57fa2e19fcd9dd24eb86309108cb02cf536
EUNSEO78 <yyy3182@naver.com>
2024-07-24 14:46:16 +0900

    Add files via upload

---
+++ Day04/condition/index.js
@@ -0,0 +1,33 @@
+// condition
+
+// const number = Number(prompt("숫자 5를 눌러뵈!"));
+// if (number == 5) {
+//   alert("5를 눌렀군요");
+// }
+
+// const number = Number(prompt("숫자 입력"));
+// if (number > 0) {
+//   alert("양수이군요");
+// }
+
+// const num = Number(prompt("숫자 입력"));
+// if (num > 0) {
+//   alert("양수");
+// } else if (num == 0) {
+//   alert("0");
+// } else {
+//   alert("음수");
+// }
+
+const age = Number(prompt("나이 입력"));
+if (age >= 20) {
+  alert("성인");
+} else if (age >= 17) {
+  alert("고등학생");
+} else if (age >= 14) {
+  alert("중학생");
+} else if (age >= 8) {
+  alert("초등학생");
+} else {
+  alert("어린이");
+}
